f66727c3475bd8b8603df5439253a7dc0bd422be
hasibrahat10 <hasibrahat10@gmail.com>
2022-01-16 01:27:38 +0600

    runner change

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/java/features"},
-        tags = {"@test-02"},
+        tags = "@test-02",
         glue = "stepDefinitions",
         plugin = {"pretty", "html:target/cucumber-html", "json:target/cucumber.json"}
 )
